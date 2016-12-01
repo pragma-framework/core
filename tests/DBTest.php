358288eef678ad51d147b667d852a0b9d3c14c1c
Simon Lieb <pips@e5150.fr>
2016-12-02 00:05:05 +0100

    Test DB::query() method - Multiple test cases
    It covers SELECT, INSERT, UPDATE, DELETE.
    
    May serve as mock up design for other test classes.

--- tests/DBTest.php
+++ tests/DBTest.php
@@ -54,5 +54,146 @@ class DBTest extends \PHPUnit_Extensions_Database_TestCase
 		$this->assertInstanceOf('PDO', $this->db->getPDO(), 'DB::getPDO() must returns PDO object');
 	}
 
+	public function testQuerySelect()
+	{
+		$this->assertDataSetsEqual(new \PHPUnit_Extensions_Database_DataSet_ArrayDataSet(array(
+			'testtable' => array(
+				array('id' => 1, 'value' => 'foo'),
+				array('id' => 2, 'value' => 'bar'),
+				array('id' => 3, 'value' => 'baz'),
+				array('id' => 4, 'value' => 'xyz'),
+			),
+		)), $this->getConnection()->createDataSet(), 'Pre-Condition: SELECT');
+
+		$this->db->query('SELECT * FROM `testtable`');
+		$this->db->query('SELECT * FROM `testtable` WHERE value = :val', array(':val' => array('bar', \PDO::PARAM_STR)));
+		$this->db->query('SELECT * FROM `testtable` WHERE value = :val', array(':val' => array('abc', \PDO::PARAM_STR)));
+
+		$this->assertDataSetsEqual(new \PHPUnit_Extensions_Database_DataSet_ArrayDataSet(array(
+			'testtable' => array(
+				array('id' => 1, 'value' => 'foo'),
+				array('id' => 2, 'value' => 'bar'),
+				array('id' => 3, 'value' => 'baz'),
+				array('id' => 4, 'value' => 'xyz'),
+			),
+		)), $this->getConnection()->createDataSet(), 'SELECTs misbehave');
+	}
+
+	public function testQueryInsert()
+	{
+		$this->assertDataSetsEqual(new \PHPUnit_Extensions_Database_DataSet_ArrayDataSet(array(
+			'testtable' => array(
+				array('id' => 1, 'value' => 'foo'),
+				array('id' => 2, 'value' => 'bar'),
+				array('id' => 3, 'value' => 'baz'),
+				array('id' => 4, 'value' => 'xyz'),
+			),
+		)), $this->getConnection()->createDataSet(), 'Pre-Condition: INSERT');
+
+		$this->db->query('INSERT INTO `testtable` (`id`, `value`) VALUES (:id, :val)', array(
+			':id'   => array(NULL,  \PDO::PARAM_INT),
+			':val'  => array('abc', \PDO::PARAM_STR),
+		));
+
+		$this->assertDataSetsEqual(new \PHPUnit_Extensions_Database_DataSet_ArrayDataSet(array(
+			'testtable' => array(
+				array('id' => 1, 'value' => 'foo'),
+				array('id' => 2, 'value' => 'bar'),
+				array('id' => 3, 'value' => 'baz'),
+				array('id' => 4, 'value' => 'xyz'),
+				array('id' => 5, 'value' => 'abc'),
+			),
+		)), $this->getConnection()->createDataSet(), 'Insert a new value with null id');
+
+		$this->db->query('INSERT INTO `testtable` (`id`, `value`) VALUES (:id, :val)', array(
+			':id'   => array(7,     \PDO::PARAM_INT),
+			':val'  => array('def', \PDO::PARAM_STR),
+		));
+
+		$this->assertDataSetsEqual(new \PHPUnit_Extensions_Database_DataSet_ArrayDataSet(array(
+			'testtable' => array(
+				array('id' => 1, 'value' => 'foo'),
+				array('id' => 2, 'value' => 'bar'),
+				array('id' => 3, 'value' => 'baz'),
+				array('id' => 4, 'value' => 'xyz'),
+				array('id' => 5, 'value' => 'abc'),
+				array('id' => 7, 'value' => 'def'),
+			),
+		)), $this->getConnection()->createDataSet(), 'Insert a new value with fixed id');
+	}
+
+	public function testQueryUpdate()
+	{
+		$this->assertDataSetsEqual(new \PHPUnit_Extensions_Database_DataSet_ArrayDataSet(array(
+			'testtable' => array(
+				array('id' => 1, 'value' => 'foo'),
+				array('id' => 2, 'value' => 'bar'),
+				array('id' => 3, 'value' => 'baz'),
+				array('id' => 4, 'value' => 'xyz'),
+			),
+		)), $this->getConnection()->createDataSet(), 'Pre-Condition: UPDATE');
+
+		$this->db->query('UPDATE `testtable`  SET `value` = :val WHERE `id` = :id', array(
+			':id'   => array(3,     \PDO::PARAM_INT),
+			':val'  => array('abc', \PDO::PARAM_STR),
+		));
+
+		$this->assertDataSetsEqual(new \PHPUnit_Extensions_Database_DataSet_ArrayDataSet(array(
+			'testtable' => array(
+				array('id' => 1, 'value' => 'foo'),
+				array('id' => 2, 'value' => 'bar'),
+				array('id' => 3, 'value' => 'abc'),
+				array('id' => 4, 'value' => 'xyz'),
+			),
+		)), $this->getConnection()->createDataSet(), 'Update value by id');
+
+		$this->db->query('UPDATE `testtable`  SET `id` = :newid WHERE `id` = :oldid', array(
+			':oldid' => array(2, \PDO::PARAM_INT),
+			':newid' => array(6, \PDO::PARAM_INT),
+		));
+
+		$this->assertDataSetsEqual(new \PHPUnit_Extensions_Database_DataSet_ArrayDataSet(array(
+			'testtable' => array(
+				array('id' => 1, 'value' => 'foo'),
+				array('id' => 3, 'value' => 'abc'),
+				array('id' => 4, 'value' => 'xyz'),
+				array('id' => 6, 'value' => 'bar'),
+			),
+		)), $this->getConnection()->createDataSet(), 'Update id by id');
+	}
+
+	public function testQueryDelete()
+	{
+		$this->assertDataSetsEqual(new \PHPUnit_Extensions_Database_DataSet_ArrayDataSet(array(
+			'testtable' => array(
+				array('id' => 1, 'value' => 'foo'),
+				array('id' => 2, 'value' => 'bar'),
+				array('id' => 3, 'value' => 'baz'),
+				array('id' => 4, 'value' => 'xyz'),
+			),
+		)), $this->getConnection()->createDataSet(), 'Pre-Condition: DELETE');
+
+		$this->db->query('DELETE FROM `testtable` WHERE `id` = :id', array(
+			':id'   => array(1, \PDO::PARAM_INT),
+		));
+
+		$this->assertDataSetsEqual(new \PHPUnit_Extensions_Database_DataSet_ArrayDataSet(array(
+			'testtable' => array(
+				array('id' => 2, 'value' => 'bar'),
+				array('id' => 3, 'value' => 'baz'),
+				array('id' => 4, 'value' => 'xyz'),
+			),
+		)), $this->getConnection()->createDataSet(), 'Delete by id');
+
+		$this->db->query('DELETE FROM `testtable` WHERE `id` IN (:id1, :id2)', array(
+			':id2' => array(2, \PDO::PARAM_INT),
+			':id1' => array(4, \PDO::PARAM_INT),
+		));
+
+		$this->assertDataSetsEqual(new \PHPUnit_Extensions_Database_DataSet_ArrayDataSet(array(
+			'testtable' => array(
+				array('id' => 3, 'value' => 'baz'),
+			),
+		)), $this->getConnection()->createDataSet(), 'Delete with multiple id');
 	}
 }
