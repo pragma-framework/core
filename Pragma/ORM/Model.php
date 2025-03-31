dac14e0266460e7c7c1e7ccec1dad24963e905b6
Cyril Rouyer <cyril@netlor.fr>
2025-03-31 13:54:30 +0200

    EVO Model > jsonSerialize compatible with php8

--- Pragma/ORM/Model.php
+++ Pragma/ORM/Model.php
@@ -945,7 +945,7 @@ class Model extends QueryBuilder implements \JsonSerializable
         return $this;
     }
 
-    public function jsonSerialize()
+    public function jsonSerialize(): mixed
     {
         return $this->as_array();
     }
